123b7b28019a8aa18d33c95faae22751ab2235ae
Bruna Oliveira <69090175+bruliveira@users.noreply.github.com>
2024-01-30 16:13:24 -0300

    :hammer: Adjust

--- atv02-rpc-cliente.py
+++ atv02-rpc-cliente.py
@@ -1,14 +1,16 @@
 import xmlrpc.client
 
 servidorLocal = xmlrpc.client.ServerProxy('http://localhost:21212')
-servidorRemoto = xmlrpc.client.ServerProxy('http://localhost:21213')
+#servidorRemoto = xmlrpc.client.ServerProxy('http://localhost:21213')
 
 print(f'\n--> Servidor local')
 print(servidorLocal)
 print(f'Data e hora atual: {servidorLocal.data_hora_atual()}')
 print(f'Quantidade de chamadas recebidas: {servidorLocal.qtd_chamadas_recebidas()}')
 
+"""
 print(f'\n--> Servidor remoto')
 print(servidorRemoto)
 print(f'Data e hora atual: {servidorRemoto.data_hora_atual()}')
 print(f'Quantidade de chamadas recebidas: {servidorRemoto.qtd_chamadas_recebidas()}')
+"""
